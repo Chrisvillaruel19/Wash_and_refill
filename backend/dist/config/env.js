911ef5f18be37b6f23b1376b8822d4c9403123e7
ElenoAnzano <elenoanzanojr123@gmail.com>
2026-07-20 12:10:22 +0800

    remove dist from git tracking

--- backend/dist/config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export const ENV = {
-    PORT: process.env.PORT || 8000,
-    BACKEND_PORT: process.env.BACKEND_URL || 'http://localhost:8000',
-    DATABASE_URL: process.env.DATABASE_URL
-};
-//# sourceMappingURL=env.js.map
--- backend/dist/routes/reports.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getReports, getSalesReport, getExpenseReport, getAttendanceReport, } from "../controllers/reports.controller.js";
-const router = Router();
-router.get("/", getReports);
-router.get("/sales", getSalesReport);
-router.get("/expenses", getExpenseReport);
-router.get("/attendance", getAttendanceReport);
-export default router;
-//# sourceMappingURL=reports.routes.js.map
